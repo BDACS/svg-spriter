d9d1695f1e608c4a364bd5b7b0475e3236e70302
Jhsk <qwe6293@nate.com>
2024-01-31 14:47:08 +0900

    feat: svg to sprite

---
+++ main.js
@@ -0,0 +1,77 @@
+const fs = require("fs");
+const path = require("path");
+const SVGSpriter = require("svg-sprite");
+
+const config = {
+  mode: {
+    symbol: {
+      inline: true,
+    },
+  },
+};
+
+const transferSvgToSprite = async () => {
+  const spriter = new SVGSpriter(config);
+  const folderPath = "./icons";
+  const files = fs.readdirSync(folderPath);
+
+  files.forEach((file) => {
+    const filePath = `./${path.join(folderPath, file)}`;
+    spriter.add(filePath, null, fs.readFileSync(filePath, "utf-8"));
+  });
+
+  console.log("File Load 성공! SVG Sprite 변환이 시작됩니다.");
+  console.log(
+    "-----------------------------------------------------------------"
+  );
+  spriter.compile((error, result) => {
+    for (const mode in result) {
+      for (const resource in result[mode]) {
+        fs.mkdirSync(path.dirname(result[mode][resource].path), {
+          recursive: true,
+        });
+        fs.writeFileSync(
+          result[mode][resource].path,
+          result[mode][resource].contents
+        );
+      }
+    }
+  });
+
+  console.log("SVG Sprite 변환 완료! 파일이 생싱 또는 수정되었습니다.");
+
+  const svgCode = await fs.promises.readFile(
+    "./symbol/svg/sprite.symbol.svg",
+    "utf-8"
+  );
+  const DOMParser = require("xmldom").DOMParser;
+
+  console.log(
+    "-----------------------------------------------------------------"
+  );
+  console.log("svg sprite json 파일 수정을 시작합니다.");
+  const xmlDoc = new DOMParser().parseFromString(svgCode, "text/xml");
+
+  const svgElement = xmlDoc.getElementsByTagName("svg")[0];
+  const symbolElements = xmlDoc.getElementsByTagName("symbol");
+
+  svgElement.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svgElement.removeAttribute("width");
+  svgElement.removeAttribute("height");
+  svgElement.removeAttribute("style");
+
+  Array.from(symbolElements).forEach((symbol) => {
+    symbol.removeAttribute("fill");
+  });
+
+  const modifiedSvgCode = xmlDoc.toString();
+
+  fs.writeFileSync("./symbol/svg/sprite.symbol.svg", modifiedSvgCode, "utf-8");
+
+  console.log(
+    "-----------------------------------------------------------------"
+  );
+  console.log("svg sprite json 파일 수정을 완료하였습니다.");
+};
+
+transferSvgToSprite();
